ce7ed446a11ad6c977f5cb375e3d558e039a5f8f
ParthSQT <parth.panchal@softqubes.com>
2022-07-29 11:33:12 +0530

    removed extra code

--- src/Views/Common/InPageNav/Index.tsx
+++ src/Views/Common/InPageNav/Index.tsx
@@ -1,31 +1,6 @@
-import { Breadcrumb, Container, FloatingLabel, Form } from "react-bootstrap";
+import { Breadcrumb, Container } from "react-bootstrap";
 
 function InPageNav() {
-  const userSelection =
-    window.location.pathname === "/home" ? (
-      <div className="d-flex gap-2">
-        <span>
-          <FloatingLabel controlId="floatingSelectGrid" label="Year">
-            <Form.Select>
-              <option value="1">2022</option>
-              <option value="2">2023</option>
-              <option value="3">2024</option>
-            </Form.Select>
-          </FloatingLabel>
-        </span>
-        <span>
-          <FloatingLabel controlId="floatingSelectGrid" label="User">
-            <Form.Select>
-              <option value="1">Andrea Moreno</option>
-              <option value="2">Andrea Moreno 2</option>
-              <option value="3">Andrea Moreno 3</option>
-            </Form.Select>
-          </FloatingLabel>
-        </span>
-      </div>
-    ) : (
-      <></>
-    );
   return (
     <Container fluid={true} className="p-0 main-top-section d-flex justify-content-between">
       <Breadcrumb className="align-items-center d-flex h-100">
@@ -39,7 +14,6 @@ function InPageNav() {
               All Accounts
             </Breadcrumb.Item> */}
       </Breadcrumb>
-      {userSelection}
     </Container>
   );
 }
